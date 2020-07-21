12f59de1e409d84c7dd606dc3c254506429550e2
indranil2507 <iindranilsaha1@gmail.com>
2020-07-21 18:09:39 +0530

    Updates

---
+++ precache-manifest.fbf8f4fee579302bd40f3a05a1542044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8a6dd77bb9d5d1ea5cf755c99c83d0",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4c62c72c5a21a1eaf456",
+    "url": "/weather-app/static/css/main.9d10fb6b.chunk.css"
+  },
+  {
+    "revision": "b59130e2328402830772",
+    "url": "/weather-app/static/js/2.d182eacb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.d182eacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c62c72c5a21a1eaf456",
+    "url": "/weather-app/static/js/main.ebc84b89.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
